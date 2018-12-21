6c0fb37966586fa54bb5998800d1da2bd076702b
ebarashkin <44406131+ebarashkin@users.noreply.github.com>
2018-12-22 00:12:52 +0300

    Update testBot.py

--- testBot.py
+++ testBot.py
@@ -1,44 +1,11 @@
-import sys
-import api
-import serial
-
-PORT = '/dev/ttyUSB0'
-BAUDRATE = 9600
-
-STATIC_CMD = {}
-STATIC_CMD['Зелёный'] = 'A'
-STATIC_CMD['Красный'] = 'B'
-STATIC_CMD['Красная'] = 'C'
-STATIC_CMD['Синяя'] = 'D'
-STATIC_CMD['Помахать'] = 'F'
-STATIC_KEYBOARD = [list(STATIC_CMD.keys())[:3], list(STATIC_CMD.keys())[3:]]
-
-arduino = None
-try:
-    arduino = serial.Serial(PORT, BAUDRATE, timeout=0)
-except serial.serialutil.SerialException as e:
-    print(e)
-    sys.exit()
+import config
+import telebot
 
 telegram = api.TelegramBot('601071819:AAGf0OobZXOkfF91VZe6DR_5i60LYXlccSg')
 
-def message_handler(messages):
-    for msg in messages:
-        print(msg)
-        if '/start' in msg[1]:
-            answer = 'Welcome to my remote Arduino control!'
-            telegram.send(msg[0], answer, STATIC_KEYBOARD)
-            continue
-        if msg[1] in STATIC_CMD:
-            arduino.write(STATIC_CMD[msg[1]].encode('utf-8'))
-            continue
-        if 'Температура' in msg[1]:
-            arduino.write('E'.encode('utf-8'))
-            answer = arduino.readline().decode('utf-8')
-            if answer:
-                telegram.send(msg[0], answer)
-            continue
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
-        telegram.send(msg[0], 'Wrong command', STATIC_KEYBOARD)
-
-telegram.listen(message_handler)
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
